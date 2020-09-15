8b77fb67a07e0058e70798261e294a4c26b77803
Crowdbotics <team@crowdbotics.com>
2020-09-15 20:13:33 +0000

    Initial commit.

---
+++ backend/lavender_20302/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for lavender_20302 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lavender_20302.settings')
+
+application = get_wsgi_application()
